877a90830a55a0353f8ea72a1fe5fa2d6f1e2845
DarkNebulaaa <mason44211@gmail.com>
2026-02-21 23:12:23 +0800

    add some comment

--- main/config.h
+++ main/config.h
@@ -4,14 +4,14 @@
 
 extern const char *TAG;
 
-#define IO_STATE_ZERO GPIO_NUM_0
-#define IO_STATE_ONE  GPIO_NUM_1
-#define IO_CTRL_UP    GPIO_NUM_3
-#define IO_CTRL_STOP  GPIO_NUM_4
-#define IO_CTRL_DOWN  GPIO_NUM_5
-#define IO_INDECATOR  GPIO_NUM_7
-#define RESET_GPIO    GPIO_NUM_9
-#define PLUGIN_GPIO   GPIO_NUM_10
+#define IO_STATE_ZERO GPIO_NUM_0 //車庫門狀態腳位0
+#define IO_STATE_ONE  GPIO_NUM_1 //車庫門狀態腳位1
+#define IO_CTRL_UP    GPIO_NUM_3 //車庫門控制腳位-開啟
+#define IO_CTRL_STOP  GPIO_NUM_4 //車庫門控制腳位-停止(not used)
+#define IO_CTRL_DOWN  GPIO_NUM_5 //車庫門控制腳位-關閉
+#define IO_INDECATOR  GPIO_NUM_7 //狀態指示燈腳位
+#define RESET_GPIO    GPIO_NUM_9 //重置按鈕腳位
+#define PLUGIN_GPIO   GPIO_NUM_10 //電磁閥控制腳位
 
 /* Reset network credentials if button is pressed for more than 3 seconds and then released */
 #define RESET_NETWORK_BUTTON_TIMEOUT        5
